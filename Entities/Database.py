4b2796c1bd8735a3e2a721ac9542e78fd17f392d
Applemoon <sanyauvarov@mail.ru>
2014-10-23 19:10:03 +0400

    Clear base improved

--- Entities/Database.py
+++ Entities/Database.py
@@ -4,7 +4,8 @@ import json
 
 class Database:
 	def __init__(self):
-		self.db = MySQLdb.connect(host="localhost", user="root", passwd="1", db="tp_subd", charset='utf8')
+		self.db = MySQLdb.connect(host="localhost", user="root", passwd="1", 
+			db="tp_subd", charset='utf8')
 		self.cursor = self.db.cursor()
 		
 
@@ -17,9 +18,17 @@ class Database:
 
 
 	def clear(self):
-		sql = 'TRUNCATE TABLE Forum;' + \
+		sql = 'SET FOREIGN_KEY_CHECKS=0;' + \
+		' TRUNCATE TABLE Forum;' + \
 		' TRUNCATE TABLE User;' + \
 		' TRUNCATE TABLE Post;' + \
-		' TRUNCATE TABLE Thread;'
+		' TRUNCATE TABLE Thread;' + \
+		' SET FOREIGN_KEY_CHECKS=1;'
 		data = self.execute(sql)
 		return [json.dumps({"code": 0, "response": "OK"})]
+
+# TODO
+# Exception _mysql_exceptions.OperationalError: 
+# (2013, 'Lost connection to MySQL server during query') 
+# in <bound method Cursor.__del__ of <MySQLdb.cursors.Cursor object at 0x7f380ccd73d0>>
+# ignored
