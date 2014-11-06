976bc65cec7d2c9fb55f0aab86133724824f451e
Applemoon <sanyauvarov@mail.ru>
2014-11-06 06:57:15 +0400

    0.1.9

---
+++ Entities/MyDatabase.py
@@ -0,0 +1,36 @@
+import MySQLdb
+import json
+
+
+class MyDatabase:
+	def __init__(self):
+		self.db = MySQLdb.connect(host="localhost", user="root", passwd="1", 
+			db="tp_subd", use_unicode=1, charset='utf8')
+		self.cursor = self.db.cursor()
+		
+
+	def execute(self, sql, args = (), post = False):
+		self.cursor.execute(sql, args)
+		if post:
+			self.db.commit()
+		self.db.close()
+		return self.cursor.fetchall()
+		
+
+	def clear(self):
+		sql = 'SET FOREIGN_KEY_CHECKS=0;' + \
+		' TRUNCATE TABLE Forum;' + \
+		' TRUNCATE TABLE User;' + \
+		' TRUNCATE TABLE Post;' + \
+		' TRUNCATE TABLE Thread;' + \
+		' SET FOREIGN_KEY_CHECKS=1;'
+		data = self.execute(sql, ())
+		return [json.dumps({"code": 0, "response": "OK"})]
+
+
+
+# TODO
+# Exception _mysql_exceptions.OperationalError: 
+# (2013, 'Lost connection to MySQL server during query') 
+# in <bound method Cursor.__del__ of <MySQLdb.cursors.Cursor object at 0x7f380ccd73d0>>
+# ignored
